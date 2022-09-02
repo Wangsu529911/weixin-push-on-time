b77bbdd25114cbe65e4da418f5ec0d3f6e0daab9
xiaoBunny <112704341+xiaoBunny@users.noreply.github.com>
2022-09-02 21:12:07 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ export const config = {
     TEMPLATE_ID: "Tp3Fmyxt5yvY-Lwe5KenzCD4E6rQTrVdtm6BwrTCFgA",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "",
+    //CALLBACK_TEMPLATE_ID: "",
 
 
     /**
